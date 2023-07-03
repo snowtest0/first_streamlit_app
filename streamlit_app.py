ea00890d1c847a5a9be578dae575e51a48f68fcb
snowtest0 <138237688+snowtest0@users.noreply.github.com>
2023-07-03 13:29:08 -0500

    Update streamlit_app.py
    pandas as pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ streamlit.text(' 🥑🍞chai')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
