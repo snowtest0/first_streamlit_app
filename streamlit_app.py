ccfb9efa3f7b4515ed7bd3715c51f9d4c02672c3
snowtest0 <138237688+snowtest0@users.noreply.github.com>
2023-07-03 10:42:06 -0500

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy Diner')
-streamlit.header('Breakfast menu')
-streamlit.text('cereal')
-streamlit.text('spinach')
-streamlit.text('chai')
+streamlit.title('My mom's new healthy Diner')
+streamlit.header('Breakfast favorites')
+streamlit.text(' 🥣 cereal')
+streamlit.text(' 🥗 spinach')
+streamlit.text(' 🥑🍞chai')
 
